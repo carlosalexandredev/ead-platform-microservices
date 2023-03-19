2641a89f8bcd59cdbdc88a1de520b384f8b9a54f
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-03-19 17:02:58 -0300

    Configuring the Authuser service's connection to the database

--- authuser/src/main/java/com/ead/authuser/AuthUserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthUserApplication {
+public class AuthuserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthUserApplication.class, args);
+        SpringApplication.run(AuthuserApplication.class, args);
     }
 
 }

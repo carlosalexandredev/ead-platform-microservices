208402339c3eff84816c66b276a55539dab15562
carlosalexandredev <carlosalexandre5670@gmail.com>
2023-05-01 18:25:22 -0300

    User Instructor Type for Creating Courses.

---
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -0,0 +1,44 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.InstructorDTO;
+import com.ead.authuser.dtos.UserDTO;
+import com.ead.authuser.enuns.UserStatus;
+import com.ead.authuser.enuns.UserType;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.service.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Log4j2
+@RestController
+@RequestMapping("/instructors")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class InstructorController {
+
+    @Autowired
+    UserService userService;
+    @PostMapping("/subscription")
+    public ResponseEntity<Object> saveSubscriptionInstructor(
+            @RequestBody @Valid InstructorDTO instructorDTO) {
+        Optional<UserModel> userModelOptional = userService.findById(instructorDTO.getUserId());
+        if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not foud.");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setUserType(UserType.INSTRUCTOR);
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+    }
+
+
+}
